1c33fea6e5e2a67484d81fc4c3e23cffc9ba0eee
Peter Njeim <peterthegreatpeterthegreat@hotmail.com>
2023-02-27 00:27:57 -0400

    fix: update __layout.svelte to +layout.svelte in lang.sh

--- lang.sh
+++ lang.sh
@@ -31,15 +31,15 @@ for file in $I18N_PATH/*.json; do
 done
 
 #* STEP 3
-map_first_line=$(awk '/'"languageMap = \{"'/,/'"\};"'/ {printf NR "\n"}' src/routes/__layout.svelte | head -n 1)
-map_last_line=$(awk '/'"languageMap = \{"'/,/'"\};"'/ {printf NR "\n"}' src/routes/__layout.svelte | tail -n 1 | head -n 1)
-sed -i "${map_first_line},${map_last_line}d" src/routes/__layout.svelte
-sed -i "${map_first_line}i let languageMap = {" src/routes/__layout.svelte
+map_first_line=$(awk '/'"languageMap = \{"'/,/'"\};"'/ {printf NR "\n"}' src/routes/+layout.svelte | head -n 1)
+map_last_line=$(awk '/'"languageMap = \{"'/,/'"\};"'/ {printf NR "\n"}' src/routes/+layout.svelte | tail -n 1 | head -n 1)
+sed -i "${map_first_line},${map_last_line}d" src/routes/+layout.svelte
+sed -i "${map_first_line}i let languageMap = {" src/routes/+layout.svelte
 
-items_first_line=$(awk '/'"languageItems = \["'/,/'"\];"'/ {printf NR "\n"}' src/routes/__layout.svelte | head -n 1)
-items_last_line=$(awk '/'"languageItems = \["'/,/'"\];"'/ {printf NR "\n"}' src/routes/__layout.svelte | tail -n 1 | head -n 1)
-sed -i "${items_first_line},${items_last_line}d" src/routes/__layout.svelte
-sed -i "${items_first_line}i languageItems = [" src/routes/__layout.svelte
+items_first_line=$(awk '/'"languageItems = \["'/,/'"\];"'/ {printf NR "\n"}' src/routes/+layout.svelte | head -n 1)
+items_last_line=$(awk '/'"languageItems = \["'/,/'"\];"'/ {printf NR "\n"}' src/routes/+layout.svelte | tail -n 1 | head -n 1)
+sed -i "${items_first_line},${items_last_line}d" src/routes/+layout.svelte
+sed -i "${items_first_line}i languageItems = [" src/routes/+layout.svelte
 
 i18n_first_line=$(awk '/'"loaders: \["'/,/'"\],"'/ {printf NR "\n"}' src/lib/i18n/i18n.ts | head -n 1)
 i18n_last_line=$(awk '/'"loaders: \["'/,/'"\],"'/ {printf NR "\n"}' src/lib/i18n/i18n.ts | tail -n 1 | head -n 1)
@@ -52,18 +52,18 @@ for file in $I18N_PATH/*.json; do
     if ! [[ " $skipped_languages " =~ " $filename " ]]; then
         langname=$(node -e "console.log(new Intl.DisplayNames(['$filename'], { type: 'language' }).of('$filename'));")
         direction=$(node -e "if (new RegExp('^[^\u0591-\u07FF\u200F\u202B\u202E\uFB1D-\uFDFD\uFE70-\uFEFC]*?[\u0591-\u07FF\u200F\u202B\u202E\uFB1D-\uFDFD\uFE70-\uFEFC]').test('$langname')) {console.log('rtl')} else {console.log('ltr')};")
-        sed -i "${map_first_line}s"'/$/'"${langname}: ['$filename', '$direction'],"'/' src/routes/__layout.svelte
-        sed -i "${items_first_line}s"'/$/'"{ $langname: \$t('$filename') },"'/' src/routes/__layout.svelte
+        sed -i "${map_first_line}s"'/$/'"${langname}: ['$filename', '$direction'],"'/' src/routes/+layout.svelte
+        sed -i "${items_first_line}s"'/$/'"{ $langname: \$t('$filename') },"'/' src/routes/+layout.svelte
         sed -i "${i18n_first_line}s"'/$/'"{locale: '$filename',key: '',loader: async () => (await import('.\/$filename.json')).default},"'/' src/lib/i18n/i18n.ts
     fi
 done
-sed -i "${map_first_line}s"'/$/'"};"'/' src/routes/__layout.svelte
-sed -i "${items_first_line}s"'/$/'"];"'/' src/routes/__layout.svelte
+sed -i "${map_first_line}s"'/$/'"};"'/' src/routes/+layout.svelte
+sed -i "${items_first_line}s"'/$/'"];"'/' src/routes/+layout.svelte
 sed -i "${i18n_first_line}s"'/$/'"],"'/' src/lib/i18n/i18n.ts
 
 #* STEP 4
 prettier --write ./src/lib/i18n/i18n.ts
-prettier --write ./src/routes/__layout.svelte
+prettier --write ./src/routes/+layout.svelte
 
 shopt -u nullglob
 
